42f52443b0c41e78c80bbad0a72b40242da26fd2
Eva-Mykhno <154619097+Eva-Mykhno@users.noreply.github.com>
2025-02-12 17:12:14 +0200

    update BookCard

--- src/components/BookCard/BookCard.jsx
+++ src/components/BookCard/BookCard.jsx
@@ -1,3 +1,60 @@
+// import { useDispatch, useSelector } from "react-redux";
+// import { toast } from "react-toastify";
+// import { addBookToLibrary, fetchUserBooks } from "../../redux/books/operations";
+// import { selectUserLibraryBooks } from "../../redux/books/selectors";
+// import LazyImage from "../LazyImage/LazyImage";
+// import s from "./BookCard.module.css";
+// import "react-toastify/dist/ReactToastify.css";
+
+// const toastConfig = {
+//   position: "top-center",
+//   autoClose: 5000,
+//   hideProgressBar: false,
+//   closeOnClick: true,
+//   pauseOnHover: true,
+//   draggable: true,
+//   theme: "dark",
+// };
+
+// const BookCard = ({ book, closeModal }) => {
+//   const dispatch = useDispatch();
+//   const userLibraryBooks = useSelector(selectUserLibraryBooks);
+
+//   const handleAddToLibrary = async (bookId) => {
+//     await dispatch(fetchUserBooks());
+//     const bookExists = userLibraryBooks.some((b) => b.title === book.title);
+
+//     if (bookExists) {
+//       toast.error("This book is already in your library", toastConfig);
+//       return;
+//     }
+//     await dispatch(addBookToLibrary(bookId));
+//     closeModal();
+//     toast.success(
+//       "The book has been successfully added to your library",
+//       toastConfig
+//     );
+//   };
+
+//   return (
+//     <div className={s.book}>
+//       <LazyImage src={book.imageUrl} alt={book.title} className={s.img} />
+//       <h3 className={s.title}>{book.title}</h3>
+//       <p className={s.author}>{book.author}</p>
+//       <p className={s.pages}>{book.totalPages} pages</p>
+//       <button
+//         className={s.buttonAdd}
+//         type="button"
+//         onClick={() => handleAddToLibrary(book._id)}>
+//         Add to library
+//       </button>
+//     </div>
+//   );
+// };
+
+// export default BookCard;
+
+import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { toast } from "react-toastify";
 import { addBookToLibrary, fetchUserBooks } from "../../redux/books/operations";
@@ -19,21 +76,26 @@ const toastConfig = {
 const BookCard = ({ book, closeModal }) => {
   const dispatch = useDispatch();
   const userLibraryBooks = useSelector(selectUserLibraryBooks);
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleAddToLibrary = async (bookId) => {
+    setIsLoading(true);
     await dispatch(fetchUserBooks());
     const bookExists = userLibraryBooks.some((b) => b.title === book.title);
 
     if (bookExists) {
       toast.error("This book is already in your library", toastConfig);
+      setIsLoading(false);
       return;
     }
+
     await dispatch(addBookToLibrary(bookId));
     closeModal();
     toast.success(
       "The book has been successfully added to your library",
       toastConfig
     );
+    setIsLoading(false);
   };
 
   return (
@@ -45,7 +107,8 @@ const BookCard = ({ book, closeModal }) => {
       <button
         className={s.buttonAdd}
         type="button"
-        onClick={() => handleAddToLibrary(book._id)}>
+        onClick={() => handleAddToLibrary(book._id)}
+        disabled={isLoading}>
         Add to library
       </button>
     </div>
